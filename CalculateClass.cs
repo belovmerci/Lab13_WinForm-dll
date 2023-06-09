b98b7fde8788240e7ef224e5d5f51a5874f8a578
Arty <belovaa@mer.ci.nsu.ru>
2023-06-09 18:47:21 +0700

    Add project files.

---
+++ CalculateClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+
+namespace CalculateLib
+{
+    public class Calculations
+    {
+        public static List<string> CalculateSumSeries(double x, double eps)
+        {
+            int a = 2;
+            double lastAns = 1;
+            double currAns = (-1) * x / a;
+            List<string> answer = new List<string>();
+            do
+            {
+                answer.Add(Convert.ToString(lastAns));
+                lastAns = currAns;
+                a++;
+                currAns = Math.Pow((-1), ((a - 1) % 2)) * (x / a);
+            } while ((Math.Abs(currAns) - Math.Abs(lastAns)) > eps);
+            return answer;
+        }
+
+        public static double CalculateNatLog(double x)
+        {
+            double answer = Math.Log((1 + x) / x);
+            return answer;
+        }
+
+    }
+}
